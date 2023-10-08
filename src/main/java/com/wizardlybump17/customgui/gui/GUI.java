2e9645cdab99a5873c170cd70702ec18bd49df40
WizardlyBump17 <davicraft130@gmail.com>
2023-10-08 14:51:55 -0300

    added the GUI#elements field

--- src/main/java/com/wizardlybump17/customgui/gui/GUI.java
+++ src/main/java/com/wizardlybump17/customgui/gui/GUI.java
@@ -1,4 +1,14 @@
 package com.wizardlybump17.customgui.gui;
 
+import com.wizardlybump17.customgui.element.Element;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.List;
+
+@Data
 public class GUI {
+
+    @NonNull
+    private final List<@NonNull Element> elements;
 }
