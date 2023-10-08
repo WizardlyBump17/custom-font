767017c52820d6f79c85d5da336d58f43765d299
WizardlyBump17 <davicraft130@gmail.com>
2023-10-07 23:13:22 -0300

    init project

---
+++ src/main/java/com/wizardlybump17/customgui/CustomGUIPlugin.java
@@ -0,0 +1,10 @@
+package com.wizardlybump17.customgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomGUIPlugin extends JavaPlugin {
+
+    public static CustomGUIPlugin getInstance() {
+        return getPlugin(CustomGUIPlugin.class);
+    }
+}
