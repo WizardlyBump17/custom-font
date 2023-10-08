333d8ba9c64f7083b0e071d701bd98fcf2d72964
WizardlyBump17 <davicraft130@gmail.com>
2023-10-08 00:20:16 -0300

    added some builder methods to the Provider class

--- src/main/java/com/wizardlybump17/customgui/font/provider/Provider.java
+++ src/main/java/com/wizardlybump17/customgui/font/provider/Provider.java
@@ -1,4 +1,16 @@
 package com.wizardlybump17.customgui.font.provider;
 
+import lombok.NonNull;
+
 public abstract class Provider {
+
+    @NonNull
+    public static BitMap.BitMapBuilder bitMap() {
+        return BitMap.builder();
+    }
+
+    @NonNull
+    public static Space.SpaceBuilder space() {
+        return Space.builder();
+    }
 }
